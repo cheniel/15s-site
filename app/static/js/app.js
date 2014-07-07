9308d1d9e8957091292af8a517383b778b7c5975
Kevin Guh <kevinjguh@yahoo.com>
2014-07-07 13:06:58 -0400

    working registration

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,28 +1,51 @@
 (function(){
 	var angularApp = angular.module('angularApp', []);
-	var errorMessages = {}
+	var errorMessages = {};
 	var xmlhttp;	
 
 	angularApp.controller('validationController', function($scope){
+		
 		this.validate = function($scope){
-			if(window.XMLHttpRequest){
-				xmlhttp = XMLHttpRequest();
-			}else{
-				xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
-			}
-			xmlhttp.onreadystatechange = function(){
-				if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
-					errorMessages = xmlhttp.responseText;
-				}
-			}
-			xmlhttp.open('POST', 'validation.py', true);
-			xmlhttp.send();
-			alert('shit');
-			alert('last alert');
+			
+			
+			alert('Thanks for registering!');
+			return true;
 		}
-	});
-	
-	angularApp.controller('liveFeedController', function(){
 
+		this.submitForm = function($scope){
+			alert('got here');
+			var firstname = $('#first-name').val();
+			var lastname = $('#last-name').val();
+			var email = $('#email').val();
+			var affiliation = $('#affiliation').val();
+
+			if(this.validate()){
+				$.ajax({
+					url: 'https://docs.google.com/forms/d/1E7P0fDQl4Jsz0Pgkmeer9FBhYfV24wFH-SG25iPq5ts/formResponse',
+					data: {'entry.1': firstname,
+						   'entry.2': lastname,
+						   'entry.3': email,
+						   'entry.4': affiliation
+						  },
+					type: 'POST',
+					dataType: 'xml',
+					statusCode: {
+						0: function(){
+							$('#first-name').val('');
+							$('#last-name').val('');
+							$('#email').val('');
+							$('#affiliation').val('');
+						},
+						200: function(){
+							$('#first-name').val('');
+							$('#last-name').val('');
+							$('#email').val('');
+							$('#affiliation').val('');
+						}
+					}
+				});
+			}else{
+
+			}
 	});
 })();
