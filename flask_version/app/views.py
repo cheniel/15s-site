be6ba943a6af7736743a3d57492b1abb4736c0d1
Kevin Guh <kevinjguh@yahoo.com>
2014-10-20 15:29:23 +0200

    need: script to confirm user registration

---
+++ flask_version/app/views.py
@@ -0,0 +1,36 @@
+from app import website
+from flask import render_template, url_for, request, redirect, session, g
+from forms import RegistrationForm
+import gdata.spreadsheet.service
+
+@website.route('/')
+@website.route('/index')
+def index():
+	registration = RegistrationForm(request.form)
+	success = False
+	return render_template('index.html', registration_form = registration, success = success)
+
+@website.route('/register', methods=['GET', 'POST'])
+def register():
+	registration_form = RegistrationForm(request.form)
+	success = True
+	
+	if request.method == 'POST' and registration_form.validate():
+		spreadsheet_client = gdata.spreadsheet.service.SpreadsheetsService()
+		spreadsheet_client.debug = True
+		spreadsheet_client.email = 'dartmouthhackathon@gmail.com'
+		spreadsheet_client.password = 'darthack'
+		spreadsheet_client.source = ''
+		spreadsheet_client.ProgrammaticLogin()
+
+		registrant = {
+						'firstname' : registration_form.first_name.data,
+						'lastname' : registration_form.last_name.data,
+						'email' : registration_form.email.data,
+						'affiliation' : registration_form.affiliation.data
+				   	}
+
+		spreadsheet_client.InsertRow(registrant, '1p8tMDMKyOpzPSGC4f9hzHQy6SPC4JsFtNNKkJOidjh0', 'od6')
+	else:
+		success = False
+	return render_template('index.html', registration_form = registration_form, success = success)
