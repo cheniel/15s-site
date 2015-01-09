e37c788e07ce6c48dbd70d15f2f63baf653be4c1
Kevin Guh <kevinjguh@yahoo.com>
2015-01-09 00:53:08 -0500

    changed forms to AJAX submission; need to work on backend

--- flask_version/app/static/js/app.js
+++ flask_version/app/static/js/app.js
@@ -1,51 +1,93 @@
-(function(){
-	var angularApp = angular.module('angularApp', []);
-	var errorMessages = {};
-	var xmlhttp;	
+$(function() {
+	$(".error").hide();
+	
+	$(".submit-button").click(function(){
+		$(".error").hide();
 
-	angularApp.controller('validationController', function($scope){
-		
-		this.validate = function($scope){
-			
-			
-			alert('Thanks for registering!');
-			return true;
+		var form_complete = true;
+		var first_name = $("input#first_name").val();
+		if(first_name == ""){
+			$("label#first_name_error").show();
+			$("input#first_name").focus();
+			form_complete = false;
 		}
 
-		this.submitForm = function($scope){
-			alert('got here');
-			var firstname = $('#first-name').val();
-			var lastname = $('#last-name').val();
-			var email = $('#email').val();
-			var affiliation = $('#affiliation').val();
 
-			if(this.validate()){
-				$.ajax({
-					url: 'https://docs.google.com/forms/d/1E7P0fDQl4Jsz0Pgkmeer9FBhYfV24wFH-SG25iPq5ts/formResponse',
-					data: {'entry.1': firstname,
-						   'entry.2': lastname,
-						   'entry.3': email,
-						   'entry.4': affiliation
-						  },
-					type: 'POST',
-					dataType: 'xml',
-					statusCode: {
-						0: function(){
-							$('#first-name').val('');
-							$('#last-name').val('');
-							$('#email').val('');
-							$('#affiliation').val('');
-						},
-						200: function(){
-							$('#first-name').val('');
-							$('#last-name').val('');
-							$('#email').val('');
-							$('#affiliation').val('');
-						}
-					}
-				});
-			}else{
+		var last_name = $("input#last_name").val();
+		if(last_name ==  ""){
+			$("label#last_name_error").show();
+			$("input#last_name").focus();
+			form_complete = false;
+		}
 
+		var email = $("input#email").val();
+		if(email == ""){
+			$("label#email_error").html("This field is required");
+			$("label#email_error").show();
+			$("input#email").focus();
+			form_complete = false;
+		}else{
+			var proper_email = false;
+			for(var i = 0; i < email.length; i++){
+				if(email[i] == "@"){
+					proper_email = true;
+					break;
+				}
 			}
+			if(!proper_email){
+				$("label#email_error").html("Hmm this doesn't look like a proper address");
+				$("label#email_error").show();
+				$("input#email").focus();
+				form_complete = false;
+			}
+		}
+
+		if(form_complete){
+			var affiliation = $("input#affiliation").val();
+			var gender = $("input#gender").val();
+			var additional_info = $("textarea#additional_info").val();
+			var first_time = $("input#first_time").val();
+			var formData = "first_name=" + first_name + "&last_name=" + last_name + "&email=" + email
+			+ "&affiliation=" + affiliation + "&gender=" + gender + "&additional_info=" + additional_info + "&first_time=" + first_time;
+
+			$("#registration_form").html("<div id='success_msg'></div>");
+			$("#success_msg").html("<p class='lead'>Thank you for your submission!</p>")
+			.append("<p>We will get back to you ASAP regarding your registration status. In the meanwhile, feel free to <a href='mailto:dartmouthhackathon@gmail.com'>contact us</a> if you have any questions.</p>")
+			.hide()
+			.fadeIn(1500);
+
+			/*$.ajax({
+				type: "POST",
+				url: $SCRIPT_ROOT + "/data_submission",
+				contentType: "application/json; charset=utf-8",
+				data: {
+					first_name: first_name,
+					last_name: last_name,
+					email: email,
+					affiliation: $("input#affiliation").val(),
+					gender: $("input#gender").val(),
+					additional_info: $("textarea#additional_info").val(),
+					first_time: $("input#first_time").val()
+				},
+				success: function() {
+					$("#registration_form").html("<div id='success_msg'></div>");
+					$("#success_msg").html("<p class='lead'>Thank you for your submission!</p>")
+					.append("<p>We will get back to you ASAP regarding your registration status. In the meanwhile, feel free to <a href='mailto:dartmouthhackathon@gmail.com'>contact us</a> if you have any questions.</p>")
+					.hide()
+					.fadeIn(1500);
+				},
+				error: function() {
+					$("#registration_form").html("<div id='fail_msg'></div<");
+					$("#fail_msg").html("<p class='lead'>Oops!</p>")
+					.append("<p>It looks like something went awry...Please try again and if the error
+					persists, let us know at dartmouthhackathon@gmail.com.</p>")
+					.hide()
+					.fadeIn(1500);
+				}
+			});*/
+		}
+
+
+		return false;
 	});
-})();
+});
--- flask_version/app/views.py
+++ flask_version/app/views.py
@@ -1,6 +1,7 @@
 from app import website
-from flask import render_template, url_for, request, redirect, session, g
+from flask import render_template, url_for, request, redirect, session, g, jsonify
 from forms import RegistrationForm
+import cgi
 import gdata.spreadsheet.service
 
 @website.route('/')
@@ -10,11 +11,12 @@ def index():
 	success = False
 	return render_template('index.html', registration_form = registration, success = success)
 
-@website.route('/register', methods=['GET', 'POST'])
+"""@website.route('/register', methods=['GET', 'POST'])
 def register():
-	registration_form = RegistrationForm(request.form)
-	success = True
-	
+
+	#registration_form = RegistrationForm(request.form)
+
+
 	if request.method == 'POST' and registration_form.validate():
 		spreadsheet_client = gdata.spreadsheet.service.SpreadsheetsService()
 		spreadsheet_client.debug = True
@@ -31,6 +33,6 @@ def register():
 				   	}
 
 		spreadsheet_client.InsertRow(registrant, '1p8tMDMKyOpzPSGC4f9hzHQy6SPC4JsFtNNKkJOidjh0', 'od6')
-	else:
-		success = False
-	return render_template('index.html', registration_form = registration_form, success = success)
+
+	return """
+	#return render_template('index.html', registration_form = registration_form, success = success)
